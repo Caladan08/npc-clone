a2ac382d771a3964f0293dc624aee45eb3b4f64f
Caladan8 <tnambiar2008@gmail.com>
2023-09-06 22:32:24 +0530

    Adjust spacing on navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,7 +1,7 @@
-<div class="h-48 py-4 flex justify-center bg-white">
-  <div class="navbarContent">
-    <div class="logo flex items-center gap-4">
-      <img src="/NPLLogo.png" alt="npl logo">
+<div class="h-48 p-4 px-12 flex justify-center bg-white">
+  <div class="navbarContent w-full">
+    <div class="logo flex items-center gap-4 h-full">
+      <img src="/NPLLogo.png" alt="npl logo" class="h-32">
       <div class="logoText">
         <h1 class="text-primary font-bold text-2xl">newcastle <br> premier <br> league t20</h1>
       </div>
