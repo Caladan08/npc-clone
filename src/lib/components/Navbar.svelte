1dd996248577058b902a532335dab975c0b1db29
Caladan8 <tnambiar2008@gmail.com>
2023-09-06 22:22:40 +0530

    Start with navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,16 @@
+<div class="h-48 py-4 flex justify-center bg-white">
+  <div class="navbarContent">
+    <div class="logo flex items-center gap-4">
+      <img src="/NPLLogo.png" alt="npl logo">
+      <div class="logoText">
+        <h1 class="text-primary font-bold text-2xl">newcastle <br> premier <br> league t20</h1>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  * {
+    text-transform: uppercase;
+  }
+</style>
