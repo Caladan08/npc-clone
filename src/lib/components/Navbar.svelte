10194e645daed9c465feed79cc6f32f7777b8970
Caladan8 <tnambiar2008@gmail.com>
2023-09-07 22:42:36 +0530

    Add underline on hover for navbar items

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import ChevronDown from "carbon-icons-svelte/lib/ChevronDown.svelte";
+	import ChevronDown from 'carbon-icons-svelte/lib/ChevronDown.svelte';
 </script>
 
 <div class="h-48 p-4 px-12 flex justify-center bg-white">
@@ -13,39 +13,65 @@
 		<div class="navbarItems h-full flex items-center">
 			<ul class="flex gap-4">
 				<li>
-          <a href="#">Home</a>
-        </li>
+					<a href="#">Home</a>
+				</li>
 				<li class="flex gap-2 items-center">
-          <a href="#">Matches</a>
-          <ChevronDown />
-        </li>
+					<a href="#">Matches</a>
+					<ChevronDown />
+				</li>
 				<li class="flex gap-2 items-center">
-          <a href="#">Stats</a>
-          <ChevronDown />
-        </li>
+					<a href="#">Stats</a>
+					<ChevronDown />
+				</li>
 				<li class="flex gap-2 items-center">
-          <a href="#">Points Table</a>
-          <ChevronDown />
-        </li>
-        <li class="flex gap-2 items-center">
-          <a href="#">Teams</a>
-          <ChevronDown />
-        </li>
-        <li class="flex gap-2 items-center">
-          <a href="#">Sponsors</a>
-          <ChevronDown />
-        </li>
-        <li class="flex gap-2 items-center">
-          <a href="#">Gallery</a>
-          <ChevronDown />
-        </li>
+					<a href="#">Points Table</a>
+					<ChevronDown />
+				</li>
+				<li class="flex gap-2 items-center">
+					<a href="#">Teams</a>
+					<ChevronDown />
+				</li>
+				<li class="flex gap-2 items-center">
+					<a href="#">Sponsors</a>
+					<ChevronDown />
+				</li>
+				<li class="flex gap-2 items-center">
+					<a href="#">Gallery</a>
+					<ChevronDown />
+				</li>
 			</ul>
 		</div>
 	</div>
 </div>
 
 <style>
-  * {
-    text-transform: uppercase;
-  }
+	* {
+		text-transform: uppercase;
+	}
+	a {
+		color: #090164;
+		text-transform: uppercase;
+		text-decoration: none;
+		letter-spacing: 0.15em;
+
+		display: inline-block;
+		padding: 15px 20px;
+		position: relative;
+	}
+	a:after {
+		background: none repeat scroll 0 0 transparent;
+		bottom: 0;
+		content: '';
+		display: block;
+		height: 2px;
+		left: 50%;
+		position: absolute;
+		background-color: #552121;
+		transition: width 0.3s ease 0s, left 0.3s ease 0s;
+		width: 0;
+	}
+	a:hover:after {
+		width: 100%;
+		left: 0;
+	}
 </style>
