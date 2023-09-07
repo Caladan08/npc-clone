0a46efe9a348649eceaa57df4fe034a6e7225c38
Caladan8 <tnambiar2008@gmail.com>
2023-09-07 22:39:45 +0530

    Use custom html for navbar items

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,21 +1,47 @@
 <script lang="ts">
-  import NavbarItem from "./NavbarItem.svelte";
+  import ChevronDown from "carbon-icons-svelte/lib/ChevronDown.svelte";
 </script>
 
 <div class="h-48 p-4 px-12 flex justify-center bg-white">
-  <div class="navbarContent w-full flex justify-between">
-    <div class="logo flex items-center gap-4 h-full">
-      <img src="/NPLLogo.png" alt="npl logo" class="h-32">
-      <div class="logoText">
-        <h1 class="text-primary font-bold text-xl">newcastle <br> premier <br> league t20</h1>
-      </div>
-    </div>
-    <div class="navbarItems h-full flex items-center">
-      <NavbarItem value="about">
-        Test
-      </NavbarItem>
-    </div>
-  </div>
+	<div class="navbarContent w-full flex justify-between">
+		<div class="logo flex items-center gap-4 h-full">
+			<img src="/NPLLogo.png" alt="npl logo" class="h-32" />
+			<div class="logoText">
+				<h1 class="text-primary font-bold text-xl">newcastle <br /> premier <br /> league t20</h1>
+			</div>
+		</div>
+		<div class="navbarItems h-full flex items-center">
+			<ul class="flex gap-4">
+				<li>
+          <a href="#">Home</a>
+        </li>
+				<li class="flex gap-2 items-center">
+          <a href="#">Matches</a>
+          <ChevronDown />
+        </li>
+				<li class="flex gap-2 items-center">
+          <a href="#">Stats</a>
+          <ChevronDown />
+        </li>
+				<li class="flex gap-2 items-center">
+          <a href="#">Points Table</a>
+          <ChevronDown />
+        </li>
+        <li class="flex gap-2 items-center">
+          <a href="#">Teams</a>
+          <ChevronDown />
+        </li>
+        <li class="flex gap-2 items-center">
+          <a href="#">Sponsors</a>
+          <ChevronDown />
+        </li>
+        <li class="flex gap-2 items-center">
+          <a href="#">Gallery</a>
+          <ChevronDown />
+        </li>
+			</ul>
+		</div>
+	</div>
 </div>
 
 <style>
