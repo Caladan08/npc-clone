13ba7d9d9d66a4c7bcbffb13afb0992460680b71
Caladan8 <tnambiar2008@gmail.com>
2023-09-08 22:34:06 +0530

    Fix arrangement of letters in logo text

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,7 +4,7 @@
 
 <div class="h-48 p-4 px-12 flex justify-center bg-white">
 	<div class="navbarContent w-full flex justify-between">
-		<div class="logo flex items-center gap-4 h-full">
+		<div class="logo flex items-center gap-4 h-full max-w-xs w-full">
 			<img src="/NPLLogo.png" alt="npl logo" class="h-32" />
 			<div class="logoText w-full">
 				<h1 class="text-primary font-bold text-xl">newcastle <br /> premier <br /> league t20</h1>
