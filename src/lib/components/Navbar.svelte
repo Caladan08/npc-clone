44c48211bad1740d05b1ba032535d3e53a72f548
Caladan8 <tnambiar2008@gmail.com>
2023-09-08 22:28:45 +0530

    Update spacing on navbar items

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -6,38 +6,32 @@
 	<div class="navbarContent w-full flex justify-between">
 		<div class="logo flex items-center gap-4 h-full">
 			<img src="/NPLLogo.png" alt="npl logo" class="h-32" />
-			<div class="logoText">
+			<div class="logoText w-full">
 				<h1 class="text-primary font-bold text-xl">newcastle <br /> premier <br /> league t20</h1>
 			</div>
 		</div>
 		<div class="navbarItems h-full flex items-center">
-			<ul class="flex gap-4">
-				<li>
+			<ul class="flex gap-1">
+				<li class="flex items-center">
 					<a href="#">Home</a>
 				</li>
 				<li class="flex gap-2 items-center">
-					<a href="#">Matches</a>
-					<ChevronDown />
+					<a href="#" class="flex gap-2 items-center">Matches <ChevronDown /></a>
 				</li>
 				<li class="flex gap-2 items-center">
-					<a href="#">Stats</a>
-					<ChevronDown />
+					<a href="#" class="flex gap-2 items-center">Stats <ChevronDown /></a>
 				</li>
 				<li class="flex gap-2 items-center">
-					<a href="#">Points Table</a>
-					<ChevronDown />
+					<a href="#" class="flex gap-2 items-center">Points Table <ChevronDown /></a>
 				</li>
 				<li class="flex gap-2 items-center">
-					<a href="#">Teams</a>
-					<ChevronDown />
+					<a href="#" class="flex gap-2 items-center">Teams <ChevronDown /></a>
 				</li>
-				<li class="flex gap-2 items-center">
+				<li class="flex items-center">
 					<a href="#">Sponsors</a>
-					<ChevronDown />
 				</li>
 				<li class="flex gap-2 items-center">
-					<a href="#">Gallery</a>
-					<ChevronDown />
+					<a href="#" class="flex gap-2 items-center">Gallery <ChevronDown /></a>
 				</li>
 			</ul>
 		</div>
@@ -52,10 +46,7 @@
 		color: #090164;
 		text-transform: uppercase;
 		text-decoration: none;
-		letter-spacing: 0.15em;
-
-		display: inline-block;
-		padding: 15px 20px;
+		padding: 12px 10px;
 		position: relative;
 	}
 	a:after {
@@ -66,7 +57,7 @@
 		height: 2px;
 		left: 50%;
 		position: absolute;
-		background-color: #552121;
+		background-color: #ee1924;
 		transition: width 0.3s ease 0s, left 0.3s ease 0s;
 		width: 0;
 	}
