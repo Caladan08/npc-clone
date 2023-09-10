7b3706b075bed40f2acab431e21c95bbc21d4018
Caladan8 <tnambiar2008@gmail.com>
2023-09-10 13:51:55 +0530

    Add svg form of footer waves

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,4 @@
+<div class="footer bg-primary w-full">
+  <img src="/FooterDesign.png" alt="footer design">
+  <h1>ABOUT</h1>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import '../app.postcss';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <div class="bg-primary h-full">
 	<Navbar />
 	<slot />
+	<Footer />
 </div>
