44a74150a0b13ab6ea0e5a31068699bb3fcd84e3
Caladan8 <tnambiar2008@gmail.com>
2023-09-10 13:48:24 +0530

    Add hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div
+	class="h-full bg-[url('/HeroImage.jpg')] bg-cover bg-center overflow-hidden bg-no-repeat uppercase flex justify-center items-center flex-col gap-8"
+>
+  <h1 class="text-secondary text-7xl">npl-2</h1>
+  <h1 class="text-tertiary text-7xl">qualifying final matches on </h1>
+  <h1 class="text-white text-7xl">09 september 2023</h1>
+</div>
