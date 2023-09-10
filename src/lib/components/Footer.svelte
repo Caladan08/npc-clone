ad024ee62d643637e5feb582227b08cc0db30f2d
Caladan8 <tnambiar2008@gmail.com>
2023-09-10 13:55:46 +0530

    Use png version of footer waves

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
 <div class="footer bg-primary w-full">
-  <img src="/FooterDesign.png" alt="footer design">
+  <img src="/FooterDesign.png" alt="footer design" class="w-full">
   <h1>ABOUT</h1>
 </div>
