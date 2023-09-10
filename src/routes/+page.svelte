2836370e5e3cfa6eaf5ab2a7456d902f456f5104
Caladan8 <tnambiar2008@gmail.com>
2023-09-10 22:35:44 +0530

    Add sponsors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,28 @@
-<div
-	class="h-full bg-[url('/HeroImage.jpg')] bg-cover bg-center overflow-hidden bg-no-repeat uppercase flex justify-center items-center flex-col gap-8"
->
-  <h1 class="text-secondary text-7xl">npl-2</h1>
-  <h1 class="text-tertiary text-7xl">qualifying final matches on </h1>
-  <h1 class="text-white text-7xl">09 september 2023</h1>
+<div class="heroSection h-full">
+	<div
+		class="h-full bg-[url('/HeroImage.jpg')] bg-cover bg-center overflow-hidden bg-no-repeat uppercase flex justify-center items-center flex-col gap-8"
+	>
+		<h1 class="text-secondary text-7xl">npl-2</h1>
+		<h1 class="text-tertiary text-7xl">qualifying final matches on</h1>
+		<h1 class="text-white text-7xl">09 september 2023</h1>
+	</div>
+</div>
+
+<div class="sponsors w-full flex flex-col items-center uppercase p-12 gap-12 bg-background">
+  <h1 class="text-primary text-5xl">sponsors</h1>
+  <div class="flex gap-4">
+    <div>
+      <img src="/SponsorOne.png" alt="sponsor one">
+    </div>
+    <div>
+      <img src="/SponsorTwo.png" alt="sponsor one">
+    </div>
+    <div>
+      <img src="/SponsorThree.png" alt="sponsor one">
+    </div>
+    <div>
+      <img src="/SponsorFour.png" alt="sponsor one">
+    </div>
+  </div>
+  <a href="#" class="py-4 px-36 bg-primary hover:bg-secondary transition-all text-white uppercase font-extralight cursor-pointer">more</a>
 </div>
