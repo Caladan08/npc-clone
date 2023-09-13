21e8bb204bea3206a11884b160f8dd128e2f6b33
Caladan8 <tnambiar2008@gmail.com>
2023-09-13 16:15:08 +0530

    Add teams section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,24 @@
+<script lang="ts">
+  import { Grid } from "@svelteuidev/core";
+
+  let teamsSources = [
+    "/teams/AgencyXI.png",
+    "/teams/BlueStallions.png",
+    "/teams/BlueTigers.png",
+    "/teams/BrownMunde.png",
+    "/teams/NewcastleFalcons.png",
+    "/teams/NewcastleHunters.png",
+    "/teams/NewcastleQalandars.png",
+    "/teams/ElermoreValeLions.png",
+    "/teams/KnightsWatchmen.png",
+    "/teams/MaitlandMonsters.png",
+    "/teams/MightyDarkHorses.png",
+    "/teams/TheFineLegs.png",
+    "/teams/TuranaTitans.png",
+    "/teams/WallsendTigers.png"
+  ]
+</script>
+
 <div class="heroSection h-full">
 	<div
 		class="h-full bg-[url('/HeroImage.jpg')] bg-cover bg-center overflow-hidden bg-no-repeat uppercase flex justify-center items-center flex-col gap-8"
@@ -25,4 +46,15 @@
     </div>
   </div>
   <a href="#" class="py-4 px-36 bg-primary hover:bg-secondary transition-all text-white uppercase font-extralight cursor-pointer">more</a>
+</div>
+
+<div class="teams bg-background p-12 px-48 uppercase flex flex-col items-center gap-12">
+  <h1 class="text-primary text-5xl">teams</h1>
+  <Grid cols={7} spacing={50}>
+    {#each teamsSources as teamSrc}
+      <Grid.Col span={1}>
+        <img src={teamSrc} alt={teamSrc} class="w-32 h-32" />
+      </Grid.Col>
+    {/each}
+  </Grid>
 </div>
